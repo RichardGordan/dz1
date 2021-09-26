98ee00664f3bcfe5af77c4fc396b2c4c515fe031
Pavel Shetkov <fertym99@gmail.com>
2021-09-26 17:01:00 +0300

    dz

---
+++ task.js
@@ -0,0 +1,34 @@
+'use strict'
+function solveEquation(a, b, c) {
+	function solveEquation(a, b, c) {
+    let arr = [];
+    if (a < 0) {
+      return false;
+    }
+    let x1;
+    let x2;
+    let discriminant = (b ** 2 - 4 * a * c);
+  
+    if (discriminant < 0) {
+      return arr;
+    } else if (discriminant = 0) {
+      return x1 = arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+       
+    } else if (discriminant > 0) {
+      x1 = arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+      x2 = arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+      return x1 , x2;
+    }
+  
+    return arr; // array
+  }
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  
+
+  return totalAmount;
+}
+
+
